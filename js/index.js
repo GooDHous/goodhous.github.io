1631749f9abc0fddbc47425117fcc40047968936
GooDHous <165495399+GooDHous@users.noreply.github.com>
2025-08-06 20:36:57 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -16,18 +16,6 @@
 
 
 
-// Старый способ загрузки файла
-
-// function download(){
-//     var element = document.createElement('a');
-//     element.setAttribute('href', 'V1.7.zip');
-//     element.setAttribute('download', 'V1.7.zip');
-//     element.style.display = 'none';
-//     document.body.appendChild(element);
-//     element.click();
-//     document.body.removeChild(element);
-// }
-
 function redirect(togo){
     window.location.replace(togo);
     document.location.href=togo
