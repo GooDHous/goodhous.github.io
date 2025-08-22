822a64aeae620b8e9805b8c18ac02f9c1bd12779
GooDHous <165495399+GooDHous@users.noreply.github.com>
2025-08-22 21:53:43 +0300

    Добавлена функция Скролла для шапки

--- js/index.js
+++ js/index.js
@@ -21,3 +21,12 @@ function redirect(togo){
     document.location.href=togo
 }
 
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
