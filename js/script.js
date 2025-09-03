270bac374805fa5c081dc4634bcc5419c337e7ec
GooDHous <165495399+GooDHous@users.noreply.github.com>
2025-09-03 11:42:00 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ new Snow ({
         icon:'<div style="border-radius:50%; display:flex; width:16px; height:16px; background-color:white;"> <img src="img/sf.ico" height="16px" width"16px"" alt="">',
 		showSnowBallsIsMobile: false,
 		showSnowflakes: true,
-		countSnowflake: 80,
+		countSnowflake: 40,
 		snowBallsLength: 50,
 		snowBallIterations: 1000,
 		snowBallupNum: 2,
